16fc80c2968366743d7ee4d917f0e398263c5298
Nizwarax <nixulrix@gmail.com>
2025-12-03 00:51:23 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://github.com/XTLS/Xray-core/releases/download/v25.10.15/Xray-android-arm64-v8a.zip
+unzip Xray-android-arm64-v8a.zip
+cp xray $PREFIX/bin/
+chmod +x $PREFIX/bin/xray
+rm Xray-android-arm64-v8a.zip geoip.dat geosite.dat
